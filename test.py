8b531436f76a95d9f0ead3d8727a53daf75a48fb
Fabian Pedregosa <f@bianp.net>
2015-11-16 10:54:48 +0100

    added tests

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ from gdprox import fmin_cgprox
 n_samples, n_features = 100, 10
 X = np.random.randn(n_samples, n_features)
 y = np.sign(np.random.randn(n_samples))
-alpha = 0.
 
 
 def test_optimize():
@@ -20,25 +19,34 @@ def test_optimize():
     def g_prox(x, alpha):
         return x
     w_ = fmin_cgprox(
-        logloss, fprime_logloss, g_prox, np.zeros(n_features), verbose=True,
+        logloss, fprime_logloss, g_prox, np.zeros(n_features),
         rtol=1e-12)
     out = optimize.fmin_l_bfgs_b(
         logloss, np.zeros(n_features), fprime=fprime_logloss)
     assert linalg.norm(out[0] - w_) < 1e-3
 
 
-# def test_sklearn():
-#     def g_prox(x, step_size):
-#         """
-#         prox of el1
-#         """
-#         return np.fmax(x - step_size * alpha, 0) - \
-#             np.fmax(- x - step_size * alpha, 0)
+def test_sklearn():
+    for alpha in np.logspace(-3, 3):
 
-#     clf = logistic.LogisticRegression(
-#         penalty='l2', fit_intercept=False, C=1e100, solver='lbfgs')
-#     clf.fit(X, y)
-#     w_ = fmin_cgprox(
-#         logloss, fprime_logloss, g_prox, np.zeros(n_features), verbose=True)
-#     assert linalg.norm(w_ - clf.coef_) < 1e-3
+        def logloss(x):
+            return logistic._logistic_loss(x, X, y, 0.)
+
+        def fprime_logloss(x):
+            return logistic._logistic_loss_and_grad(x, X, y, 0.)[1]
+
+        def g_prox(x, step_size):
+            """
+            L1 regularization
+            """
+            return np.fmax(x - step_size * alpha, 0) - \
+                np.fmax(- x - step_size * alpha, 0)
+
+        clf = logistic.LogisticRegression(
+            penalty='l1', fit_intercept=False, C=1 / alpha)
+        clf.fit(X, y)
+        w_ = fmin_cgprox(
+            logloss, fprime_logloss, g_prox, np.zeros(n_features),
+            rtol=1e-12)
+        assert linalg.norm(w_ - clf.coef_) < 1e-3
 
