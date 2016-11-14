21cc085d82b7cbee96bfd728aaa6a86df06b2a4a
Fabian Pedregosa <f@bianp.net>
2016-11-14 09:28:41 +0100

    more

--- copt/__init__.py
+++ copt/__init__.py
@@ -3,4 +3,4 @@ __version__ = '0.4.dev0'
 from .gradient_descent import proximal_gradient
 from .prox_tv import prox_tv2d, prox_tv1d
 from .three_split import davis_yin
-from .primal_dual import condat_vu
+# from .primal_dual import condat_vu
