b8217e43b7e15974c62d788f01139ac7b2076260
Fabian Pedregosa <f@bianp.net>
2016-11-17 10:45:02 +0100

    FIX: wrong import

--- examples/plot_tv_comparison.py
+++ examples/plot_tv_comparison.py
@@ -10,7 +10,7 @@ import pylab as plt
 colors = ['#7fc97f', '#beaed4', '#fdc086']
 
 
-from copt.prox_tv import prox_tv2d, prox_tv1d_rows, prox_tv1d_cols
+from copt.total_variation import prox_tv2d, prox_tv1d_rows, prox_tv1d_cols
 from copt import three_split, proximal_gradient
 from copt.utils import Trace
 from copt.datasets import load_img1
