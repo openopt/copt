5a94fa9bbdd5c6488e5d11299f655a8e1fb7fc67
Fabian Pedregosa <f@bianp.net>
2017-01-10 18:12:17 +0100

    started work on PSSAGA

--- copt/stochastic.py
+++ copt/stochastic.py
@@ -44,8 +44,8 @@ def deriv_logistic(w, x, y):
 
 def fmin_SAGA(
         fun, fun_deriv, A, b, x0, step_size=None, g_prox=None, n_jobs=1,
-        max_iter=1000, tol=1e-6, verbose=True, callback=None, epochs_per_iter=5,
-        trace=False):
+        max_iter=1000, tol=1e-6, verbose=True, callback=None, trace=False,
+        step_size_factor=4):
     """Stochastic average gradient augumented (SAGA) algorithm.
 
     The SAGA algorithm can solve optimization problems of the form
@@ -86,14 +86,12 @@ def fmin_SAGA(
         fun = f_logistic
         fun_deriv = deriv_logistic
         if step_size is None:
-            max_rows = norm_rows(A)
-            step_size = 4.0 / max_rows
+            step_size = 4.0 / (norm_rows(A) * step_size_factor)
     elif fun == 'squared':
         fun = f_squared
         fun_deriv = deriv_squared
         if step_size is None:
-            max_rows = norm_rows(A)
-            step_size = 1.0 / max_rows
+            step_size = 1.0 / (norm_rows(A) * step_size_factor)
     elif hasattr(fun, '__call__'):
         pass
     else:
@@ -104,10 +102,10 @@ def fmin_SAGA(
 
     if sparse.issparse(A):
         A = sparse.csr_matrix(A)
-        epoch_iteration, trace_loss = _epoch_factory_sparse(
+        epoch_iteration, trace_loss = _epoch_factory_sparse_SAGA(
             fun, fun_deriv, A, b)
     else:
-        epoch_iteration, trace_loss = _epoch_factory_dense(
+        epoch_iteration, trace_loss = _epoch_factory_SAGA(
             fun, fun_deriv, A, b)
 
     start_time = datetime.now()
@@ -139,10 +137,10 @@ def fmin_SAGA(
             print(it, norm_grad)
         if norm_grad < tol:
             success = True
-            print('Breaking')
             break
     if trace:
-        print('Computing trace')
+        if verbose:
+            print('.. computing trace ..')
         # .. compute function values ..
         with futures.ThreadPoolExecutor(max_workers=n_jobs) as executor:
             trace_fun = [t for t in executor.map(trace_loss, trace_x)]
@@ -151,7 +149,105 @@ def fmin_SAGA(
         x=x, success=success, nit=it, trace_fun=trace_fun, trace_time=trace_time)
 
 
-def _epoch_factory_dense(fun, f_prime, A, b):
+
+def fmin_PSSAGA(
+        fun, fun_deriv, A, b, x0, step_size=None, g_prox=None, h_prox=None,
+        max_iter=1000, tol=1e-6, verbose=True, callback=None, trace=False,
+        step_size_factor=4):
+    """Stochastic average gradient augumented (SAGA) algorithm.
+
+    The SAGA algorithm can solve optimization problems of the form
+
+        argmin_x \frac{1}{n} \sum_{i=1}^n f(a_i^T x, b_i) + g(x)
+
+    Parameters
+    ----------
+    fun: callable or string
+        XXX
+
+    fun_deriv: callable or None
+        f_prime(a_i^T x, b_i) returns the (scalar) derivative of f with
+        respect to its first argument.
+
+    Returns
+    -------
+    res : OptimizeResult
+        The optimization result represented as a
+        ``scipy.optimize.OptimizeResult`` object. Important attributes are:
+        ``x`` the solution array, ``success`` a Boolean flag indicating if
+        the optimizer exited successfully and ``message`` which describes
+        the cause of the termination. See `scipy.optimize.OptimizeResult`
+        for a description of other attributes.
+
+    References
+    ----------
+    Defazio, Aaron, Francis Bach, and Simon Lacoste-Julien. "SAGA: A fast
+    incremental gradient method with support for non-strongly convex composite
+    objectives." Advances in Neural Information Processing Systems. 2014.
+    """
+
+    x = np.ascontiguousarray(x0).copy()
+    assert x.size == A.shape[1]
+    assert A.shape[0] == b.size
+
+    if fun == 'logistic':
+        fun = f_logistic
+        fun_deriv = deriv_logistic
+        if step_size is None:
+            step_size = 4.0 / (norm_rows(A) * step_size_factor)
+    elif fun == 'squared':
+        fun = f_squared
+        fun_deriv = deriv_squared
+        if step_size is None:
+            step_size = 1.0 / (norm_rows(A) * step_size_factor)
+    elif hasattr(fun, '__call__'):
+        pass
+    else:
+        raise NotImplementedError
+
+    n_samples, n_features = A.shape
+    success = False
+
+    epoch_iteration, trace_loss = _epoch_factory_PSSAGA(
+            fun, fun_deriv, A, b)
+
+    start_time = datetime.now()
+    trace_fun = []
+    trace_time = []
+    trace_x = []
+
+    # .. memory terms ..
+    memory_gradient = np.zeros(n_samples)
+    gradient_average = np.zeros(n_features)
+
+    # .. iterate on epochs ..
+    for it in range(max_iter):
+        epoch_iteration(x, memory_gradient, gradient_average,
+                    np.random.permutation(n_samples), step_size)
+        if callback is not None:
+            callback(x)
+        if trace:
+            trace_x.append(x.copy())
+            trace_time.append((datetime.now() - start_time).total_seconds())
+
+        grad_map = np.linalg.norm(gradient_average)
+        if verbose:
+            print('Iteration %s, gradient mapping norm %s' % (it, grad_map))
+        if grad_map < tol:
+            success = True
+            break
+    if trace:
+        if verbose:
+            print('.. computing trace ..')
+        # .. compute function values ..
+        with futures.ThreadPoolExecutor(max_workers=n_jobs) as executor:
+            trace_fun = [t for t in executor.map(trace_loss, trace_x)]
+
+    return optimize.OptimizeResult(
+        x=x, success=success, nit=it, trace_fun=trace_fun, trace_time=trace_time)
+
+
+def _epoch_factory_SAGA(fun, f_prime, A, b):
 
     @njit(nogil=True, cache=True)
     def epoch_iteration_template(
@@ -176,7 +272,7 @@ def _epoch_factory_dense(fun, f_prime, A, b):
     return epoch_iteration_template, full_loss
 
 
-def _epoch_factory_sparse(fun, f_prime, A, b):
+def _epoch_factory_sparse_SAGA(fun, f_prime, A, b):
 
     A_data = A.data
     A_indices = A.indices
@@ -224,3 +320,27 @@ def _epoch_factory_sparse(fun, f_prime, A, b):
 
     return epoch_iteration_template, full_loss
 
+
+def _epoch_factory_PSSAGA(fun, f_prime, A, b):
+
+    @njit(nogil=True, cache=True)
+    def epoch_iteration_template(
+            x, memory_gradient, gradient_average, sample_indices,
+            step_size):
+        n_samples, n_features = A.shape
+        # .. inner iteration ..
+        for i in sample_indices:
+            grad_i = f_prime(x, A[i], b[i])
+            incr = (grad_i - memory_gradient[i]) * A[i]
+            x -= step_size * (incr + gradient_average)
+            gradient_average += incr / n_samples
+            memory_gradient[i] = grad_i
+
+    @njit(nogil=True, cache=True)
+    def full_loss(x):
+        obj = 0.
+        n_samples, n_features = A.shape
+        for i in range(n_samples):
+            obj += fun(x, A[i], b[i]) / n_samples
+
+    return epoch_iteration_template, full_loss
