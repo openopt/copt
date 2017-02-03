bd56cc7e59799374ea5b2687b5522a2405b890db
Fabian Pedregosa <f@bianp.net>
2017-02-03 13:49:23 +0100

    Example of asynchronous optimization

--- examples/plot_asynchronous.py
+++ examples/plot_asynchronous.py
@@ -4,18 +4,14 @@ Asynchronous Stochastic Gradient
 
 """
 import numpy as np
-from scipy import linalg
 import pylab as plt
-colors = ['#7fc97f', '#beaed4', '#fdc086']
-
-
-from sklearn import datasets
 from copt import stochastic
 from scipy import sparse
+colors = ['#7fc97f', '#beaed4', '#fdc086']
 
 # generate a random large sparse matrix as input data
 # and associated target labels
-n_samples, n_features = 50000, 10000
+n_samples, n_features = 10000, 10000
 X = sparse.random(n_samples, n_features, density=0.001, format='csr')
 w = sparse.random(1, n_features, density=0.01).toarray().ravel()
 y = np.sign(X.dot(w) + np.random.randn(n_samples))
@@ -24,20 +20,36 @@ y = np.sign(X.dot(w) + np.random.randn(n_samples))
 alpha = 1.0 / n_samples
 beta = 1.0 / n_samples
 step_size = stochastic.compute_step_size('logistic', X, alpha)
-opt = stochastic.fmin_SAGA(
+
+max_iter = 25
+
+opt_1cores = stochastic.fmin_SAGA(
     stochastic.f_logistic, stochastic.deriv_logistic, X, y, np.zeros(X.shape[1]),
-    step_size=step_size, alpha=alpha, beta=beta, max_iter=20,
+    step_size=step_size, alpha=alpha, beta=beta, max_iter=max_iter, tol=-1,
     trace=True, verbose=True, g_prox=stochastic.prox_L1, g_func=stochastic.f_L1)
 
 
-opt_4cores = stochastic.fmin_SAGA(
-    stochastic.f_logistic, stochastic.deriv_logistic, X, y, np.zeros(X.shape[1]), step_size=step_size,
-    alpha=alpha, beta=beta, tol=0, max_iter=20,
-    trace=True, verbose=True, g_prox=stochastic.prox_L1, g_func=stochastic.f_L1, n_jobs=4)
+opt_2cores = stochastic.fmin_SAGA(
+    stochastic.f_logistic, stochastic.deriv_logistic, X, y, np.zeros(X.shape[1]),
+    step_size=step_size, alpha=alpha, beta=beta, max_iter=max_iter, tol=-1,
+    trace=True, verbose=True, g_prox=stochastic.prox_L1, g_func=stochastic.f_L1, n_jobs=2)
 
-fmin = min(np.min(opt.trace_func), np.min(opt_4cores.trace_func))
-plt.plot(opt.trace_time, opt.trace_func - fmin, lw=4, label='1 core')
-plt.plot(opt_4cores.trace_time, opt_4cores.trace_func - fmin, lw=4, label='4 cores')
+
+opt_3cores = stochastic.fmin_SAGA(
+    stochastic.f_logistic, stochastic.deriv_logistic, X, y, np.zeros(X.shape[1]),
+    step_size=step_size, alpha=alpha, beta=beta, max_iter=max_iter, tol=-1,
+    trace=True, verbose=True, g_prox=stochastic.prox_L1, g_func=stochastic.f_L1, n_jobs=3)
+
+fmin = min(np.min(opt_1cores.trace_func), np.min(opt_2cores.trace_func),
+           np.min(opt_3cores.trace_func))
+
+plt.plot(opt_1cores.trace_time, opt_1cores.trace_func - fmin, lw=4, label='1 core',
+         color=colors[0])
+plt.plot(opt_2cores.trace_time, opt_2cores.trace_func - fmin, lw=4, label='2 cores',
+         color=colors[1])
+plt.plot(opt_3cores.trace_time, opt_3cores.trace_func - fmin, lw=4, label='3 cores',
+         color=colors[2])
+
 plt.yscale('log')
 plt.ylabel('Function suboptimality')
 plt.xlabel('Time')
