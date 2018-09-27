50eea6a015afbf99944ebbe24d3f96a91db1004b
Fabian Pedregosa <f@bianp.net>
2018-09-27 14:39:56 -0700

    FIX

--- copt/__init__.py
+++ copt/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '0.4.0-dev'
 
 from .gradient import minimize_PGD, minimize_TOS, minimize_APGD, minimize_PDHG
-from .randomized import minimize_SAGA_L1, minimize_SVRG_L1, minimize_BCD, minimize_VRTOS
+from .randomized import minimize_SAGA_L1, minimize_SVRG_L1, minimize_VRTOS
 from .frank_wolfe import minimize_FW, minimize_PFW_L1
 from . import datasets
 from . import tv_prox
