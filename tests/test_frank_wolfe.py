375df047156efaa27c7b70cc0479ea2606a829fc
Fabian Pedregosa <f@bianp.net>
2018-09-27 14:22:27 -0700

    tests PFW

--- tests/test_frank_wolfe.py
+++ tests/test_frank_wolfe.py
@@ -14,11 +14,6 @@ b = A.dot(w) + np.random.randn(n_samples)
 # greater than 1
 b = np.abs(b / np.max(np.abs(b)))
 
-
-all_solvers = (
-    ['FW', cp.minimize_FW, 2e-2],
-    # ['PFW', cp.minimize_PFW_L1, 1e-3],
-)
 loss_funcs = [
     cp.utils.LogLoss, 
     cp.utils.SquareLoss,
@@ -26,13 +21,12 @@ loss_funcs = [
 
 
 
-@pytest.mark.parametrize("name_solver, solver, tol", all_solvers)
 @pytest.mark.parametrize("loss_grad", loss_funcs)
-def test_optimize(name_solver, solver, tol, loss_grad):
+def test_FW(loss_grad):
     f_grad = loss_grad(A, b, 1./n_samples).f_grad
     alpha = 1.
     l1ball = cp.utils.L1Ball(alpha)
-    opt = solver(
+    opt = cp.minimize_FW(
         f_grad, l1ball.lmo, np.zeros(n_features), tol=0,
         max_iter=5000)
     assert np.isfinite(opt.x).sum() == n_features
@@ -41,4 +35,21 @@ def test_optimize(name_solver, solver, tol, loss_grad):
     ss = 1/L
     grad = f_grad(opt.x)[1]
     grad_map = (opt.x - l1ball.prox(opt.x - ss*grad, ss))/ss
-    assert np.linalg.norm(grad_map) < tol, name_solver
+    assert np.linalg.norm(grad_map) < 2e-2
+
+
+@pytest.mark.parametrize("loss_grad", loss_funcs)
+def test_PairwiseFW(loss_grad):
+    f_grad = loss_grad(A, b, 1./n_samples).f_grad
+    alpha = 1.
+    l1ball = cp.utils.L1Ball(alpha)
+    opt = cp.minimize_PFW_L1(
+        f_grad, np.zeros(n_features), alpha, tol=0,
+        max_iter=5000)
+    assert np.isfinite(opt.x).sum() == n_features
+
+    L = cp.utils.get_lipschitz(A, 'square', 1./n_samples)
+    ss = 1/L
+    grad = f_grad(opt.x)[1]
+    grad_map = (opt.x - l1ball.prox(opt.x - ss*grad, ss))/ss
+    assert np.linalg.norm(grad_map) < 2e-2
