07b5dde74bd766d865b839660db41605e25d7c20
Fabian Pedregosa <pedregosa@google.com>
2019-03-22 15:04:48 -0400

    formatting

--- examples/plot_group_lasso.py
+++ examples/plot_group_lasso.py
@@ -9,10 +9,10 @@ its proximal operator, which is implemented in copt through the
 function prox of object :meth:`copt.utils.GroupL1`.
 
 """
-import numpy as np
-from scipy import sparse
-import pylab as plt
 import copt as cp
+import numpy as np
+import pylab as plt
+from scipy import sparse
 
 np.random.seed(0)
 
@@ -46,31 +46,31 @@ all_betas = [0, 1e-2, 1e-1, 0.2]
 all_trace_ls, all_trace_nols = [], []
 out_img = []
 for i, beta in enumerate(all_betas):
-    print('beta = %s' % beta)
-    G1 = cp.utils.GroupL1(beta, groups)
+  print('beta = %s' % beta)
+  G1 = cp.utils.GroupL1(beta, groups)
 
-    def loss(x):
-        return f(x) + G1(x)
+  def loss(x):
+    return f(x) + G1(x)
 
-    cb_tosls = cp.utils.Trace()
-    x0 = np.zeros(n_features)
-    pgd_ls = cp.minimize_PGD(
-        f.f_grad, x0, G1.prox, step_size=step_size,
-        max_iter=max_iter, tol=1e-14, verbose=1,
-        callback=cb_tosls)
-    trace_ls = np.array([loss(x) for x in cb_tosls.trace_x])
-    all_trace_ls.append(trace_ls)
+  cb_tosls = cp.utils.Trace()
+  x0 = np.zeros(n_features)
+  pgd_ls = cp.minimize_PGD(
+      f.f_grad, x0, G1.prox, step_size=step_size,
+      max_iter=max_iter, tol=1e-14, verbose=1,
+      callback=cb_tosls)
+  trace_ls = np.array([loss(x) for x in cb_tosls.trace_x])
+  all_trace_ls.append(trace_ls)
 
-    cb_tos = cp.utils.Trace()
-    x0 = np.zeros(n_features)
-    pgd = cp.minimize_PGD(
-        f.f_grad, x0, G1.prox,
-        step_size=step_size,
-        max_iter=max_iter, tol=1e-14, verbose=1,
-        backtracking=False, callback=cb_tos)
-    trace_nols = np.array([loss(x) for x in cb_tos.trace_x])
-    all_trace_nols.append(trace_nols)
-    out_img.append(pgd.x)
+  cb_tos = cp.utils.Trace()
+  x0 = np.zeros(n_features)
+  pgd = cp.minimize_PGD(
+      f.f_grad, x0, G1.prox,
+      step_size=step_size,
+      max_iter=max_iter, tol=1e-14, verbose=1,
+      backtracking=False, callback=cb_tos)
+  trace_nols = np.array([loss(x) for x in cb_tos.trace_x])
+  all_trace_nols.append(trace_nols)
+  out_img.append(pgd.x)
 
 
 # .. plot the results ..
@@ -78,30 +78,30 @@ fig, ax = plt.subplots(2, 4, sharey=False)
 xlim = [0.02, 0.02, 0.1]
 markevery = [1000, 1000, 100, 100]
 for i, beta in enumerate(all_betas):
-    ax[0, i].set_title(r'$\lambda=%s$' % beta)
-    ax[0, i].set_title(r'$\lambda=%s$' % beta)
-    ax[0, i].plot(out_img[i])
-    ax[0, i].plot(ground_truth)
-    ax[0, i].set_ylim((-0.5, 1.5))
-    ax[0, i].set_xticks(())
-    ax[0, i].set_yticks(())
+  ax[0, i].set_title(r'$\lambda=%s$' % beta)
+  ax[0, i].set_title(r'$\lambda=%s$' % beta)
+  ax[0, i].plot(out_img[i])
+  ax[0, i].plot(ground_truth)
+  ax[0, i].set_ylim((-0.5, 1.5))
+  ax[0, i].set_xticks(())
+  ax[0, i].set_yticks(())
 
-    fmin = min(np.min(all_trace_ls[i]), np.min(all_trace_nols[i]))
-    scale = all_trace_ls[i][0] - fmin
-    plot_tos, = ax[1, i].plot(
-        (all_trace_ls[i] - fmin) / scale,
-        lw=4, marker='o', markevery=100,
-        markersize=10)
+  fmin = min(np.min(all_trace_ls[i]), np.min(all_trace_nols[i]))
+  scale = all_trace_ls[i][0] - fmin
+  plot_tos, = ax[1, i].plot(
+      (all_trace_ls[i] - fmin) / scale,
+      lw=4, marker='o', markevery=100,
+      markersize=10)
 
-    plot_nols, = ax[1, i].plot(
-        (all_trace_nols[i] - fmin) / scale,
-        lw=4, marker='h', markevery=markevery[i],
-        markersize=10)
+  plot_nols, = ax[1, i].plot(
+      (all_trace_nols[i] - fmin) / scale,
+      lw=4, marker='h', markevery=markevery[i],
+      markersize=10)
 
-    ax[1, i].set_xlabel('Iterations')
-    ax[1, i].set_yscale('log')
-    ax[1, i].set_ylim((1e-14, None))
-    ax[1, i].grid(True)
+  ax[1, i].set_xlabel('Iterations')
+  ax[1, i].set_yscale('log')
+  ax[1, i].set_ylim((1e-14, None))
+  ax[1, i].grid(True)
 
 
 plt.gcf().subplots_adjust(bottom=0.15)
