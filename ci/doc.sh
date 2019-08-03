891c83b33e5e19a0be83cf977fcf1203bb978d6b
Fabian Pedregosa <pedregosa@google.com>
2019-08-03 13:20:48 -0400

    trying other python versions

--- ci/doc.sh
+++ ci/doc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 pip install -r requirements.txt
-pip install sphinx loky joblib sphinx_copybutton memory_profiler
+pip install -U sphinx loky joblib sphinx_copybutton memory_profiler
 # I need my fork of sphinx-gallery to run stuff in parallel
 pip install git+https://github.com/fabianp/sphinx-gallery.git
 python setup.py install
