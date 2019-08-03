526472656009deddb3b1787f31bb21c7c6386af3
Fabian Pedregosa <pedregosa@google.com>
2019-08-03 12:59:53 -0400

    Faster doc build

--- ci/doc.sh
+++ ci/doc.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
 pip install -r requirements.txt
-pip install sphinx sphinx-gallery sphinx_copybutton memory_profiler
+pip install sphinx loky joblib sphinx_copybutton memory_profiler
+# I need my fork of sphinx-gallery to run stuff in parallel
+pip install git+https://github.com/fabianp/sphinx-gallery.git
 python setup.py install
 cd doc
 make html
