5c512a75306a243488ad0bbd090e94e8f3ccb323
Fabian Pedregosa <pedregosa@google.com>
2019-08-03 13:38:10 -0400

    temporary fix

--- ci/doc.sh
+++ ci/doc.sh
@@ -3,7 +3,9 @@
 pip install -r requirements.txt
 pip install -U sphinx loky joblib sphinx_copybutton memory_profiler
 # I need my fork of sphinx-gallery to run stuff in parallel
-pip install git+https://github.com/fabianp/sphinx-gallery.git
+# pip install git+https://github.com/fabianp/sphinx-gallery.git
+# get the standard one since mine is not working on Gcloud
+pip install -U sphinx-gallery
 python setup.py install
 cd doc
 make html
