3c3fe98f4c7028e863c3ecfab8242c179ef00a2a
Fabian Pedregosa <f@bianp.net>
2019-08-05 11:46:22 -0400

    build documentation

--- ci/doc.sh
+++ ci/doc.sh
@@ -8,5 +8,4 @@ pip install -U sphinx loky joblib sphinx_copybutton memory_profiler
 pip install -U sphinx-gallery
 python setup.py install
 cd doc
-make html-noplot
-# gsutil -m rsync -r -c -d _build/html/ gs://copt
+make html
