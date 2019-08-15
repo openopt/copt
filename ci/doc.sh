1460729340e07bcb6f2af576acf80cde49b62e8e
Fabian Pedregosa <pedregosa@google.com>
2019-08-15 13:13:18 -0400

    Add JAX for the examples

--- ci/doc.sh
+++ ci/doc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 pip install -r requirements.txt
-pip install -U sphinx loky joblib sphinx_copybutton memory_profiler
+pip install -U sphinx loky joblib sphinx_copybutton memory_profiler jax
 # I need my fork of sphinx-gallery to run stuff in parallel
 # pip install git+https://github.com/fabianp/sphinx-gallery.git
 # get the standard one since mine is not working on Gcloud
