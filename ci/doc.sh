f407bc45bbc219c73c80628da4be466eaef81c64
Fabian Pedregosa <f@bianp.net>
2019-08-18 13:47:58 -0400

    use forked sphinx-gallery

--- ci/doc.sh
+++ ci/doc.sh
@@ -3,9 +3,7 @@
 pip install -r requirements.txt
 pip install -U sphinx loky joblib sphinx_copybutton memory_profiler jax jaxlib
 # I need my fork of sphinx-gallery to run stuff in parallel
-# pip install git+https://github.com/fabianp/sphinx-gallery.git
-# get the standard one since mine is not working on Gcloud
-pip install -U sphinx-gallery
+pip install git+https://github.com/fabianp/sphinx-gallery.git
 python setup.py install
 cd doc
 make html
