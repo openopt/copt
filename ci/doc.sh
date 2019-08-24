2c8b3fc86453ed717e1d8b16833fca2a37825adf
Fabian Pedregosa <f@bianp.net>
2019-08-24 18:47:05 -0400

    badge for doc status

--- ci/doc.sh
+++ ci/doc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+mkdir -p _build/html/
+wget https://storage.googleapis.com/tm-github-builds/build/failure.svg -o _build/html/doc_status.svg
 pip install -r requirements.txt
 pip install -U sphinx loky joblib sphinx_copybutton memory_profiler jax jaxlib
 # I need my fork of sphinx-gallery to run stuff in parallel
@@ -9,7 +11,6 @@ cd doc
 make html
 if [ $? -eq 0 ]; then
     # set up a badge depending on the result of the build
+    rm _build/html/doc_status.svg
     wget https://storage.googleapis.com/tm-github-builds/build/success.svg -o _build/html/doc_status.svg
-else
-    wget https://storage.googleapis.com/tm-github-builds/build/failure.svg -o _build/html/doc_status.svg
 fi
