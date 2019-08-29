b3ad50e8ca80979f9cb0aa730aace14e9a5b754a
Fabian Pedregosa <pedregosa@google.com>
2019-08-29 11:13:25 -0400

    Add examples and test files to pylint

--- ci/test.sh
+++ ci/test.sh
@@ -7,7 +7,7 @@ py.test --workers auto
 
 # pylint
 pip install pylint anybadge
-pylint --rcfile=ci/pylintrc --output-format=text copt | tee pylint.txt
+pylint --rcfile=ci/pylintrc --output-format=text copt tests/*.py examples/*.py examples/*/*.py | tee pylint.txt
 score=$(sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint.txt)
 echo "Pylint score was $score"
 anybadge --value=$score --file=pylint.svg pylint
