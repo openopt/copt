3c2b7b343150d025e999a5d2dc0ebd50f2959e94
Fabian Pedregosa <pedregosa@google.com>
2019-10-03 14:30:51 -0400

    v0.8.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name="copt",
     description="Library for composite optimization in Python",
     long_description=io.open("README.rst", encoding="utf-8").read(),
-    version="0.8.0",
+    version="0.8.1",
     author="Fabian Pedregosa",
     author_email="f@bianp.net",
     url="http://pypi.python.org/pypi/copt",
