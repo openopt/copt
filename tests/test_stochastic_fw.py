76a97a9ea99d1b41c102206ab9a1e85c0a906282
GeoffNN <g.negiar@gmail.com>
2020-05-05 14:19:04 +0200

    Modified tests to account for SFW refactor

--- tests/test_stochastic_fw.py
+++ tests/test_stochastic_fw.py
@@ -15,10 +15,11 @@ b = A.dot(w) + np.random.randn(n_samples)
 b = np.abs(b / np.max(np.abs(b)))
 
 LOSS_FUNCS = [cp.utils.LogLoss]
-SFW = [cp.randomized.minimize_sfw, cp.randomized.minimize_sfw_mokhtari]
+VARIANTS = ['SAG', 'MK', 'LF']
 
-@pytest.mark.parametrize("sfw", SFW)
-def test_fw_api(sfw):
+
+@pytest.mark.parametrize("variant", VARIANTS)
+def test_fw_api(variant):
     """Check that SFW algorithms take the right arguments and raises the right exceptions."""
 
     # test that the algorithm does not fail if x0
@@ -27,7 +28,7 @@ def test_fw_api(sfw):
     cb = cp.utils.Trace(f)
     alpha = 1.0
     l1ball = cp.utils.L1Ball(alpha)
-    sfw(
+    cp.randomized.minimize_sfw(
         f.partial_deriv,
         A,
         b,
@@ -35,13 +36,14 @@ def test_fw_api(sfw):
         l1ball.lmo,
         tol=0,
         callback=cb,
+        variant=variant
         )
 
 
-@pytest.mark.parametrize("sfw", SFW)
+@pytest.mark.parametrize("variant", VARIANTS)
 @pytest.mark.parametrize("alpha", [0.1, 1.0, 10.0, 100.0])
 @pytest.mark.parametrize("loss_grad", LOSS_FUNCS)
-def test_sfw_l1(sfw, loss_grad, alpha):
+def test_sfw_l1(variant, loss_grad, alpha):
     """Test SFW algorithms with L1 constraint."""
     f = loss_grad(A, b, 1.0 / n_samples)
     cb = cp.utils.Trace(f)
@@ -54,13 +56,14 @@ def test_sfw_l1(sfw, loss_grad, alpha):
         l1ball.lmo,
         tol=1e-3,
         callback=cb,
+        variant=variant
     )
 
 
-@pytest.mark.parametrize("sfw", SFW)
+@pytest.mark.parametrize("variant", VARIANTS)
 @pytest.mark.parametrize("alpha", [0.1, 1.0, 10.0, 100.0])
 @pytest.mark.parametrize("loss_grad", LOSS_FUNCS)
-def test_sfw_gap_traceback(sfw, loss_grad, alpha):
+def test_sfw_gap_traceback(variant, loss_grad, alpha):
     """Test outputting the FW gap for SFW algorithms."""
     f = loss_grad(A, b, 1.0 / n_samples)
     l1ball = cp.utils.L1Ball(alpha)
@@ -80,7 +83,7 @@ def test_sfw_gap_traceback(sfw, loss_grad, alpha):
 
     cb = TraceGaps(f)
 
-    opt = sfw(
+    opt = cp.randomized.minimize_sfw(
         f.partial_deriv,
         A,
         b,
@@ -88,20 +91,22 @@ def test_sfw_gap_traceback(sfw, loss_grad, alpha):
         l1ball.lmo,
         tol=1e-3,
         callback=cb,
+        variant=variant
     )
 
-@pytest.mark.parametrize("sfw", SFW)
+
+@pytest.mark.parametrize("variant", VARIANTS)
 @pytest.mark.parametrize("A", [sparse.random(n_samples, n_features, 0.1,
                                              fmt)
                                for fmt in ['coo', 'csr', 'csc', 'lil']])
-def test_sfw_sparse(sfw, A):
+def test_sfw_sparse(variant, A):
     """Check that SFW algorithms run on sparse data matrices and initial values."""
 
     f = cp.utils.LogLoss(A, b, 1.0 / n_samples)
     cb = cp.utils.Trace(f)
     alpha = 1.0
     l1ball = cp.utils.L1Ball(alpha)
-    sfw(
+    cp.randomized.minimize_sfw(
         f.partial_deriv,
         A,
         b,
@@ -109,5 +114,6 @@ def test_sfw_sparse(sfw, A):
         l1ball.lmo,
         tol=0,
         callback=cb,
+        variant=variant
         )
 
