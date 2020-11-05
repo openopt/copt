14775d4d275d7b329a571a0d09a32c077f5bf629
Fabian Pedregosa <pedregosa@google.com>
2020-11-05 08:54:20 -0500

    simplify doc cloud installation

--- ci/doc.sh
+++ ci/doc.sh
@@ -2,12 +2,10 @@
 #
 mkdir -p _build/html/
 pip install -r requirements.txt
-pip install -U sphinx loky joblib sphinx_copybutton memory_profiler jax jaxlib anybadge numba
+pip install -U sphinx sphinx-gallery loky joblib sphinx_copybutton memory_profiler jax jaxlib anybadge numba
 # For pytorch examples
 pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install git+https://github.com/RobustBench/robustbench
-# fork of sphinx-gallery to run examples in parallel
-pip install git+https://github.com/fabianp/sphinx-gallery.git
 python setup.py install
 cd doc
 make html > doc_log.txt
