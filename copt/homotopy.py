2924df14ff833bfced0eb88e2b7127f5a89d93c2
Gideon Dresdner <gideond@gmail.com>
2021-03-16 13:59:33 +0100

    wip implemented lin obj / next is (smoothed) const
    need to figure out a reasonable way to implement the (smoothed)
    constraints. I think that the smoothe constraint should be part of an
    object which wraps over the various inputs of the constraint.
    
    But then, should there be a difference between equality and inequality?
    In practice, they are actually very similar.
    
    Should they all be SeparableConstraint? Need to make a list of them and
    see what they have in commmon.

--- copt/homotopy.py
+++ copt/homotopy.py
@@ -1,11 +1,15 @@
-
 """Homotopy Methods. Currently, Homotopy Conditional Gradient Methods (a.k.a. Frank-Wolfe)"""
+
+import os, sys
 import warnings
 from collections import defaultdict
 import numpy as np
 from scipy import linalg
 from scipy import optimize
+from scipy import io as sio
+import copt
 from copt import utils
+from copt import datasets
 
 
 EPS = np.finfo(np.float32).eps
@@ -17,6 +21,82 @@ def minimize_homotopy_cgm(objective_fun, smoothed_constraint_fun, x0, lmo, beta0
     pass
 
 
+def reduced_digits():
+    mat = sio.loadmat(os.path.join(datasets.DATA_DIR, "sdp_mnist", "reduced_clustering_mnist_digits.mat"))
+    C = mat['Problem']['C'][0][0]
+    return C
 
-# Need to setup the experiment here. Get the dataset described in the paper and go from there
+def full_digits():
+    mat = sio.loadmat(os.path.join(datasets.DATA_DIR, "sdp_mnist", "full_clustering_mnist_digits.mat"))
+    C = mat['Problem']['C'][0][0]
+    return C
 
+C_mat = reduced_digits()
+
+class LinearObjective:
+    def __init__(self, M):
+        self.M = M
+
+    def __call__(self, X):
+        loss,_ = self.f_grad(X)
+        return loss
+
+    def f_grad(self, X):
+        # TODO return_gradient ?
+        '''X is the decision variable.'''
+        loss = np.dot(X.flatten(), self.M.flatten())
+        grad = self.M
+        grad = grad.flatten()
+        return loss, grad
+
+    @property
+    def lipschitz(self):
+        # TODO think
+        _,D,_ = np.linalg.svd(self.M)
+        largest_eigv = max(D)
+        return largest_eigv / self.M.shape[0]
+
+
+minimize_homotopy_cgm(
+    linear_objective.f_grad,
+    EqualityConstraint, # TODO figure out some way reasonable way to implement this
+    x_init,
+    traceball.lmo,
+    tol = 0,
+    callback=cb,
+    step="sublinear",
+    max_iter=1000
+)
+
+def test_linear_objective():
+    # TODO dependency on C_mat
+    linear_objective = LinearObjective(C_mat)
+    cb = copt.utils.Trace(linear_objective)
+    alpha = 1.
+    traceball = copt.constraint.TraceBall(alpha, C_mat.shape)
+
+    x_init = np.zeros(C_mat.shape)
+    x_init = x_init.flatten()
+
+    sol = copt.minimize_frank_wolfe(
+        linear_objective.f_grad,
+        x_init,
+        traceball.lmo,
+        tol=0,
+        lipschitz=linear_objective.lipschitz,
+        callback=cb,
+        step="sublinear",
+        max_iter=1000
+    )
+
+    def check(x):
+        ss = 1 / linear_objective.lipschitz
+        _,grad = linear_objective.f_grad(x)
+        # this is the proximal mapping, zero at optimum
+        grad_map = (x - traceball.prox(x - ss * grad, ss)) / ss
+        return np.linalg.norm(grad_map)
+
+    assert check(x_init) > 0.4
+    assert check(sol.x) < 0.4
+
+test_linear_objective()
